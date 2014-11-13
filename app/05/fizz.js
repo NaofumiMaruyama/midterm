ab130b7fd89a9b5be2d06e582a7869902b5626b9
NaofumiMaruyama <maruyamanaofumi@net40-dhcp63.sfc.keio.ac.jp>
2014-11-13 11:57:49 +0900

    midterm

---
+++ app/05/fizz.js
@@ -0,0 +1,11 @@
+var fizz = function(a){
+  if(a % 3 ==0){
+    return "fizz"
+  }
+  else if(a % 3 != "fizz"){
+    return a
+  }
+  else if(a = "fizz"){
+    return a
+  }
+};
--- app/09/factorial.js
+++ app/09/factorial.js
@@ -1,1 +1,17 @@
+var factorial = function(a){
+  if(Number.isInteger(a) && a >=1)
+}
 
+var counter = 1;
+while(counter < a){
+var b;
+b = counter + 1;
+var result;
+return b*(b+1);
+}
+
+
+
+
+
+};
